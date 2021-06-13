078e8ae30c9604b62184cad637b35f9b3aca08cb
SyHoMadara <79520000+SyHoMadara@users.noreply.github.com>
2021-06-13 13:18:28 +0430

    Setting fragment class and main class configurations.

--- app/src/main/java/com/example/omarket/login/LoginActivity.java
+++ app/src/main/java/com/example/omarket/login/LoginActivity.java
@@ -1,9 +1,11 @@
-package com.example.omarket;
+package com.example.omarket.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Toast;
+
+import com.example.omarket.R;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,4 +14,5 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
 }
