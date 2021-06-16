e68f48aeabd67397c3874f1de42309e160cf894f
SyHoMadara <79520000+SyHoMadara@users.noreply.github.com>
2021-06-16 16:21:36 +0430

    user class created

---
+++ app/src/main/java/com/example/omarket/backend/user/UserInformation.java
@@ -0,0 +1,10 @@
+package com.example.omarket.backend.user;
+
+public class UserInformation {
+    public String fullName;
+    public String emailAddress;
+    public String password;
+    public String phoneNumber;
+    public boolean isSuperAdmin;
+    public boolean isAdmin;
+}
