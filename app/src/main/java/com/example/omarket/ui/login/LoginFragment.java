8dbd9faae32024dee405a7d0e48f83646bf83315
rbehzad <rbehzad1001@gmail.com>
2021-06-16 23:12:17 +0430

    add signInGoogle code in LoginFragment.java

--- app/src/main/java/com/example/omarket/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/omarket/ui/login/LoginFragment.java
@@ -1,28 +1,50 @@
 package com.example.omarket.ui.login;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.example.omarket.R;
 import com.example.omarket.ui.NavigationFragment;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import org.jetbrains.annotations.NotNull;
 
 public class LoginFragment extends NavigationFragment implements View.OnClickListener {
-
+    private GoogleSignInClient mGoogleSignInClient;
     Button loginButton;
     TextView register;
-
+    ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode()== Activity.RESULT_OK) {
+                        Intent intent = result.getData();
+                        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(intent);
+                        handleSignInResult(task);
+                    }
+                }
+            });
     @Nullable
     @org.jetbrains.annotations.Nullable
     @Override
@@ -33,10 +55,46 @@ public class LoginFragment extends NavigationFragment implements View.OnClickLis
         loginButton.setOnClickListener(this);
         register = view.findViewById(R.id.login_text_view_register);
         register.setOnClickListener(this);
-
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(
+                GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
         return view;
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // check for an existing signed-in user
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getActivity());
+        updateUI(account);
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            // signed in successfully, show authenticated UI
+            updateUI(account);
+        } catch(ApiException e) {
+            // the ApiException status code indicates the detailed failure reason
+            updateUI(null);
+        }
+    }
+    private void updateUI(GoogleSignInAccount acct) {
+        if(acct != null) {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
 
+            Toast.makeText(getActivity(), "login as " + personName, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getActivity(), "Could not sign in with google", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void signIn(View view) {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        mStartForResult.launch(signInIntent);
+    }
     @SuppressLint("ResourceType")
     @Override
     public void onClick(View v) {
