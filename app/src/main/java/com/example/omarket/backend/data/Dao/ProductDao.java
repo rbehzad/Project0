8df334a857144865c365f4fa45bfb4c3d86e2cb2
rbehzad <rbehzad1001@gmail.com>
2021-07-07 12:59:48 +0430

    ++database

--- app/src/main/java/com/example/omarket/backend/data/Dao/ProductDao.java
+++ app/src/main/java/com/example/omarket/backend/data/Dao/ProductDao.java
@@ -1,6 +1,8 @@
 package com.example.omarket.backend.data.Dao;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
 import androidx.room.Query;
 
 import com.example.omarket.backend.data.entities.Product;
@@ -14,5 +16,13 @@ public interface ProductDao {
     @Query("INSERT INTO Product VALUES(:id, :name, :info, :imagePath, :sellerName, :sellerId)")
     void insert(int id, String name, String info, String imagePath, String sellerName, String sellerId);
 
+    // get product info by searching its name
+    @Query("SELECT * FROM Product WHERE name IN (:productName) ")
+    List<Product> loadAllByIds(int[] productName);
 
+    @Insert
+    void insertAll(Product...products);
+
+    @Delete
+    void delete(Product product);
 }
--- app/src/main/java/com/example/omarket/backend/data/Dao/UserDao.java
+++ app/src/main/java/com/example/omarket/backend/data/Dao/UserDao.java
@@ -1,7 +1,27 @@
 package com.example.omarket.backend.data.Dao;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.omarket.backend.data.entities.Product;
+import com.example.omarket.backend.data.entities.User;
+
+import java.util.List;
 
 @Dao
 public interface UserDao {
+    @Query("INSERT INTO User VALUES(:id, :name, :emailAddress, :password, :phoneNumber, :userType)")
+    void insert(int id, String name, String emailAddress, String password, String phoneNumber, String userType);
+
+    // get product info by searching its name
+    @Query("SELECT * FROM User WHERE emailAddress IN (:userEmailAddress) ")
+    List<User> loadAllByIds(int[] userEmailAddress);
+
+    @Insert
+    void insertAll(User...users);
+
+    @Delete
+    void delete(User user);
 }
