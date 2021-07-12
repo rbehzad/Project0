3969eb0b4976b1c07fad890e55283051da1fc3a0
SyHoMadara <sh.sarafrazi80@gmail.com>
2021-07-12 12:50:40 +0430

    change navigation fragment

--- app/src/main/java/com/example/omarket/ui/NavigationFragment.java
+++ app/src/main/java/com/example/omarket/ui/NavigationFragment.java
@@ -11,16 +11,8 @@ import com.example.omarket.ui.main_fragments.Color;
 public abstract class NavigationFragment extends Fragment {
     protected SaveCommand saveCommand;
 
-    public NavigationFragment() {
-        this.saveCommand = SaveCommand.getInstance();
-    }
-
     public void navigateFromViewTo(View view , int id){
         Navigation.findNavController(view).navigate(id);
-    }
-
-    public void changeIconColorTo(Color color){
-
     }
 
 }
