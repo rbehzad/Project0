c189d009f133706836c608a63f131c5c826b7e8c
SyHoMadara <sh.sarafrazi80@gmail.com>
2021-07-18 15:58:27 +0430

    fragment product

---
+++ app/src/main/java/com/example/omarket/ui/productFragment/tabLayoutFragment/EditFragment.java
@@ -0,0 +1,21 @@
+package com.example.omarket.ui.productFragment.tabLayoutFragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.omarket.R;
+public class EditFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit, container, false);
+    }
+}
