d8bd0bfa4f72cac5ef8809730f62aa431c6bc5e5
SyHoMadara <sh.sarafrazi80@gmail.com>
2021-07-20 14:04:19 +0430

    register

--- app/src/main/java/com/example/omarket/ui/addProduct.java
+++ app/src/main/java/com/example/omarket/ui/addProduct.java
@@ -8,21 +8,32 @@ import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.omarket.R;
+import com.example.omarket.backend.api.APIHandler;
+import com.example.omarket.ui.main_fragments.Color;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class addProduct extends Fragment implements View.OnClickListener {
     private static int RESULT_LOAD_IMAGE = 1;
     View view;
     ImageView imageView;
+    EditText titleText, costText, descriptionText;
     Button button , btnSave;
     private static final int PICK_IMAGE = 100;
     Uri imageUri;
+    ProgressBar progressBar;
 
     public addProduct() {
         // Required empty public constructor
@@ -45,15 +56,23 @@ public class addProduct extends Fragment implements View.OnClickListener {
             imageView.setImageURI(imageUri);
         }
     }
-
+    private Animation shakeAnimation;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_product, container, false);
+        shakeAnimation = AnimationUtils.loadAnimation(view.getContext(), R.anim.shake);
+        titleText = view.findViewById(R.id.edit_text_title);
+        descriptionText = view.findViewById(R.id.edit_text_description);
+        costText = view.findViewById(R.id.edit_text_cost);
+        titleText.setAnimation(shakeAnimation);
+        costText.setAnimation(shakeAnimation);
+        descriptionText.setAnimation(shakeAnimation);
         btnSave = view.findViewById(R.id.btn_save);
         button = (Button) view.findViewById(R.id.buttonLoadPicture2);
         imageView = (ImageView) view.findViewById(R.id.addproduct_fragment_image_view_product);
+        progressBar = view.findViewById(R.id.progressBar);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,12 +83,34 @@ public class addProduct extends Fragment implements View.OnClickListener {
         return view;
     }
 
+    private void viewFailed(View v) {
+        v.startAnimation(shakeAnimation);
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btn_save:
-
+                if (titleText.getText()==null || titleText.getText().toString().trim().equals("")||
+                    descriptionText.getText()==null || descriptionText.getText().toString().trim().equals("")||
+                    costText.getText()==null || costText.getText().toString().trim().equals("")) {
+                    viewFailed(titleText);
+                    viewFailed(descriptionText);
+                    viewFailed(costText);
+                }
+                Map<String, Object> body = new HashMap<>();
+                body.put("title",titleText.getText().toString());
+                body.put("description",descriptionText.getText().toString());
+                body.put("cost",costText.getText().toString());
+                changeVisibilityTo(progressBar, View.VISIBLE);
+                APIHandler.updateUserAddProductUpdateProductApi(getActivity(),body,"AP");
+                changeVisibilityTo(progressBar, View.INVISIBLE);
                 break;
         }
     }
+
+    public void changeVisibilityTo(View v, int visibility) {
+        v.setVisibility(visibility);
+    }
+
 }
